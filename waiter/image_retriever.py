bd7eff0b7699800e27a91e4bb4ae8869945a550e
Pius Nyongesa <pinyonge@microsoft.com>
2021-02-11 12:25:17 +0300

    -Can upload images

--- waiter/image_retriever.py
+++ waiter/image_retriever.py
@@ -7,7 +7,7 @@ class ImageRetriver():
     @classmethod
     def get_image(cls, db: DatabaseProxy,id=None) -> [ImageProperties]:
         container  = db.get_container_client("images")
-        query = "SELECT c.id, c.attributes,c.created_at, c.image FROM c"
+        query = "SELECT c.id, c.attributes, c.image FROM c ORDER BY c.created_at DESC"
         items = [ImageProperties(**item) for item in container.query_items(query=query, enable_cross_partition_query=True)]
 
         return items
